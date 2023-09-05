49f9dfecc8cd9dcbc60b9982252e074620cb8e28
AlbersSoftware <Chrisalberssoftware@gmail.com>
2023-09-05 13:34:03 -0400

    profiles, data, updated dataloader class,custom meta data, and more.

--- src/main/java/com/AlbersSoftware/Contentcreatorcalendar/Application.java
+++ src/main/java/com/AlbersSoftware/Contentcreatorcalendar/Application.java
@@ -1,16 +1,19 @@
 package com.AlbersSoftware.Contentcreatorcalendar;
 
+import com.AlbersSoftware.Contentcreatorcalendar.configs.ContentCalendarProperties;
 import com.AlbersSoftware.Contentcreatorcalendar.model.Content;
 import com.AlbersSoftware.Contentcreatorcalendar.model.Status;
 import com.AlbersSoftware.Contentcreatorcalendar.model.Type;
 import com.AlbersSoftware.Contentcreatorcalendar.repository.ContentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
-
+@EnableConfigurationProperties(ContentCalendarProperties.class)
 @SpringBootApplication
 public class Application {
 
@@ -18,17 +21,7 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-    @Bean
-	CommandLineRunner commandLineRunner(ContentRepository repository){
-
-		return args -> {
-
-				Content content = new Content( null,"First Vlog","my first vlog", Status.IDEA, Type.VIDEO, LocalDateTime.now(),null,"");
-				repository.save(content);
-
-
-
-		};
-}
 
 }
+
+
